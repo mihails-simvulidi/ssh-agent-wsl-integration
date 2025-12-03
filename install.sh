07c1d923fd2e977437f03db7062fe9454ec1aa08
Mihails Simvulidi <mihails@simvulidi.lv>
2025-12-03 09:50:23 +0200

    Refactor install.sh to create systemd directory before installing relay-ssh-agent.service

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 scriptdir=$(dirname "$0")
+systemddir=~/.config/systemd/user/
 
 sudo apt-get install -y socat
 winget.exe install -e --id jstarks.npiperelay || true
 sudo install "$scriptdir/set-ssh-agent.sh" /etc/profile.d/
-install "$scriptdir/relay-ssh-agent.service" ~/.config/systemd/user/
+mkdir -p "$systemddir"
+install "$scriptdir/relay-ssh-agent.service" "$systemddir"
 systemctl --user daemon-reload
 systemctl --user enable relay-ssh-agent.service
 systemctl --user restart relay-ssh-agent.service
