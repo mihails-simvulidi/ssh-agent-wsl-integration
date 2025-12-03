ff667a2a959b34c77deff6550b444ed19c0755fb
Mihails Simvulidi <mihails@simvulidi.lv>
2025-12-03 12:24:38 +0200

    Add Restart directive to relay-ssh-agent.service for improved reliability

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ WantedBy=default.target
 [Service]
 Environment="PATH=$PATH"
 ExecStart=socat "EXEC:npiperelay.exe -p //./pipe/openssh-ssh-agent" "UNIX-LISTEN:\${XDG_RUNTIME_DIR}/ssh-agent.socket,fork"
+Restart=always
 EOF
 
 cat <<'EOF' | sudo tee /etc/profile.d/ssh-agent.sh >/dev/null
