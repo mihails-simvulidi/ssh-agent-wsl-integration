d51c758736e33a2c583e88e3723213fb08d560f6
Mihails Simvulidi <mihails@simvulidi.lv>
2025-12-03 09:33:19 +0200

    Fix winget installation command to prevent script failure

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 scriptdir=$(dirname "$0")
 
 sudo apt-get install -y socat
-winget.exe install -e --id jstarks.npiperelay
+winget.exe install -e --id jstarks.npiperelay || true
 sudo install "$scriptdir/set-ssh-agent.sh" /etc/profile.d/
 sudo install "$scriptdir/relay-ssh-agent.service" /etc/systemd/system/
 sudo systemctl daemon-reload
