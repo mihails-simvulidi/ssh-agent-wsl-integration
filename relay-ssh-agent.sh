13089b940928a74a7c5c33034de011a4f10b8cd8
Mihails Simvulidi <mihails@simvulidi.lv>
2025-12-03 11:46:52 +0200

    Fix escaping in PATH export for relay-ssh-agent.sh to ensure correct execution

--- relay-ssh-agent.sh
+++ relay-ssh-agent.sh
@@ -2,5 +2,5 @@
 set -e
 
 WINUSERPROFILE=$(cmd.exe /C ECHO %USERPROFILE% | tr -d '\r')
-export PATH="$(wslpath "$WINUSERPROFILE")/AppData/Local/Microsoft/WinGet/Packages/jstarks.npiperelay_Microsoft.Winget.Source_8wekyb3d8bbwe:$PATH"
+export PATH="$(wslpath \"$WINUSERPROFILE\")/AppData/Local/Microsoft/WinGet/Packages/jstarks.npiperelay_Microsoft.Winget.Source_8wekyb3d8bbwe:$PATH"
 exec socat "EXEC:npiperelay.exe -p //./pipe/openssh-ssh-agent" "UNIX-LISTEN:$XDG_RUNTIME_DIR/ssh-agent.socket"
