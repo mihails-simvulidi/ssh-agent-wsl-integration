43a86c51b43d16d39a00165f823f56dc22e1507a
Mihails Simvulidi <mihails@simvulidi.lv>
2025-12-03 11:09:03 +0200

    Refactor install script to use correct variables and add relay-ssh-agent.sh for SSH agent integration

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
-scriptdir=$(dirname "$0")
-systemddir=~/.config/systemd/user/
+repodir=$(dirname "$0")
+scriptinstalldir=~/.local
+systemddir=~/.config/systemd/user
 
 sudo apt-get install -y socat
 winget.exe install -e --id jstarks.npiperelay || true
-sudo install "$scriptdir/set-ssh-agent.sh" /etc/profile.d/
-mkdir -p "$systemddir"
-install "$scriptdir/relay-ssh-agent.service" "$systemddir"
+sudo install "$repodir/set-ssh-agent.sh" /etc/profile.d/
+mkdir -p "$scriptinstalldir" "$systemddir"
+install "$repodir/relay-ssh-agent.sh" "$scriptinstalldir/"
+install "$repodir/relay-ssh-agent.service" "$systemddir/"
 systemctl --user daemon-reload
 systemctl --user enable relay-ssh-agent.service
 systemctl --user restart relay-ssh-agent.service
