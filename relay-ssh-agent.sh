ec4d60b5a5de5e8ac4980b7a2d41004457dc1a7e
Mihails Simvulidi <mihails@simvulidi.lv>
2025-12-03 11:32:16 +0200

    Fix npiperelay executable path in relay-ssh-agent.sh for correct execution

--- relay-ssh-agent.sh
+++ relay-ssh-agent.sh
@@ -3,4 +3,4 @@ set -e
 
 WINUSERPROFILE=$(cmd.exe /C ECHO %USERPROFILE% | tr -d '\r')
 NPIPERELAY="$(wslpath "$WINUSERPROFILE")/AppData/Local/Microsoft/WinGet/Packages/jstarks.npiperelay_Microsoft.Winget.Source_8wekyb3d8bbwe/npiperelay.exe"
-exec socat "EXEC:npiperelay.exe -p //./pipe/openssh-ssh-agent" "UNIX-LISTEN:${XDG_RUNTIME_DIR}/ssh-agent.socket"
+exec socat "EXEC:${NPIPERELAY} -p //./pipe/openssh-ssh-agent" "UNIX-LISTEN:${XDG_RUNTIME_DIR}/ssh-agent.socket"
