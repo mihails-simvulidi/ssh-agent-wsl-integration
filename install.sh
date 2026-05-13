6d56d0f8e4a28863f37f7a5abb00de9bb208c7cc
Mihails Simvulidi <mihails@simvulidi.lv>
2026-05-13 14:49:09 +0300

    Update relay-ssh-agent.service to use verbose mode for socat and change restart policy to on-failure

--- install.sh
+++ install.sh
@@ -23,8 +23,8 @@ fi
 
 cat <<EOF > "$systemddir/relay-ssh-agent.service"
 [Service]
-ExecStart=socat -dd $(escape_for_systemd_execstart_socat_exec "$npiperelay_exe_path" -p //./pipe/openssh-ssh-agent) $(escape_for_systemd_execstart "UNIX-LISTEN:%t/ssh-agent.socket,fork")
-Restart=always
+ExecStart=socat -dd $(escape_for_systemd_execstart_socat_exec "$npiperelay_exe_path" -p -v //./pipe/openssh-ssh-agent) $(escape_for_systemd_execstart "UNIX-LISTEN:%t/ssh-agent.socket,fork")
+Restart=on-failure
 
 [Install]
 WantedBy=default.target
